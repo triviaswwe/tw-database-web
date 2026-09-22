04baee6372f1cb465d5dacff64c44320b1f56282
lucasferreyra10 <lucasferreyra10@gmail.com>
2026-09-22 20:58:54 -0300

    fix: update favicon path to use Vercel Blob

--- pages/_documents.js
+++ pages/_documents.js
@@ -2,11 +2,14 @@
 
 import { Html, Head, Main, NextScript } from 'next/document';
 
+// Tu dominio base de Vercel Blob
+const BLOB_BASE_URL = 'https://ljejfdquofuxccca.public.blob.vercel-storage.com';
+
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href={`${BLOB_BASE_URL}/favicon.ico`} />
         <meta name="theme-color" content="#000000" />
       </Head>
       <body>
